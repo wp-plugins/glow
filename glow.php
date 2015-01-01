4a707c3022e11cc0b2578a97b16d0821980082f8
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-01 14:29:22 +0000

    v2.0.0 b1 in trunk
    git-svn-id: https://plugins.svn.wordpress.org/glow/trunk@1057900 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- glow.php
+++ glow.php
@@ -3,7 +3,7 @@
 Plugin Name: Glow Javascript Library
 Plugin URI: http://www.ramoonus.nl/wordpress/glow/
 Description: This plugin adds BBCs Glow Javascript library to your set-up.
-Version: 1.7.7
+Version: 2.0.0-beta1
 Author: Ramoonus
 Author URI: http://www.ramoonus.nl/
 License: GPL2
@@ -11,12 +11,18 @@ License: GPL2
 
 // Update glow
 function rw_glow() {
+		// glow
+		wp_deregister_script('glow'); // deregister
+		wp_enqueue_script('glow', plugins_url('/js/glow.js', __FILE__), false, '2.0.0');
 		// Core
 		wp_deregister_script('glow-core'); // deregister
-		wp_enqueue_script('glow-core', plugins_url('/js/1.7.7/core/core.js', __FILE__), false, '1.7.7');
-		// Widgets
-		wp_deregister_script('glow-core'); // deregister
-		wp_enqueue_script('glow-core', plugins_url('/js/1.7.7/widgets/widgets.js', __FILE__), false, '1.7.7');
+		wp_enqueue_script('glow-core', plugins_url('/js/core.js', __FILE__), false, '2.0.0');
+		// UI
+		wp_deregister_script('glow-ui'); // deregister
+		wp_enqueue_script('glow-ui', plugins_url('/js/ui.js', __FILE__), false, '2.0.0');
+		// CSS
+		wp_deregister_style('glow-ui'); // deregister
+		wp_enqueue_style('glow-ui', plugins_url('/js/glow.js', __FILE__), false, '2.0.0');
 }
 // load
 add_action('init', 'rw_glow');
